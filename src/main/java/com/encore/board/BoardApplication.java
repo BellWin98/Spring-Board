6c6802737ede0c8195b3aed5ccd2337233cdf577
BellWin98 <jongseunghan@naver.com>
2024-01-30 17:50:10 +0900

    Session값을 In-memory가 아닌 Redis에 저장

--- src/main/java/com/encore/board/BoardApplication.java
+++ src/main/java/com/encore/board/BoardApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableJpaAuditing // JPA Auditing 어노테이션들을 모두 활성화
 @EnableScheduling // 스케줄러 사용 시 설정
+@EnableRedisHttpSession // Session Storage를 Redis로 사용하겠다.
 @SpringBootApplication
 public class BoardApplication {
 
