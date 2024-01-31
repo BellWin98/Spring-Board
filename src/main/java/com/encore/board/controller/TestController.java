8e901428fb07132c58c9aefd4f3f8ed33e5d1352
BellWin98 <jongseunghan@naver.com>
2024-01-31 12:13:16 +0900

    1.31: 로그인한 유저의 세션정보 얻는 방법

--- src/main/java/com/encore/board/controller/TestController.java
+++ src/main/java/com/encore/board/controller/TestController.java
@@ -3,9 +3,14 @@ package com.encore.board.controller;
 import com.encore.board.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @Slf4j // lombok 내 어노테이션이며, 쉽게 logback 로그 라이브러리 사용 가능
@@ -29,5 +34,25 @@ public class TestController {
     public String exceptionTestMethod1(@PathVariable Long id){
         memberService.showMemberDetails(id);
         return "ok";
+    }
+
+    @GetMapping("userinfo/test")
+    public String userInfoTest(HttpServletRequest req){
+        // 로그인 유저 정보 얻는 방식
+        // 방법1. Session에 Attribute를 통해 접근
+        String email1 = req.getSession().getAttribute("email").toString();
+        System.out.println("email1: " + email1);
+
+        // 방법2. Session에서 Authentication 객체에 접근
+        SecurityContext securityContext = (SecurityContext) req.getSession().getAttribute("SPRING_SECURITY_CONTEXT");
+        String email2 = securityContext.getAuthentication().getName();
+        System.out.println("email2: " + email2);
+
+        // 방법3. SecurityContextHolder에서 Authentication 객체에 접근 (가장 일반적 / 토큰에서 똑같이 사용)
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email3 = authentication.getName();
+        System.out.println("email3: " + email3);
+
+        return null;
     }
 }
